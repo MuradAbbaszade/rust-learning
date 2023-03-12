9f91034b26e2809e65cb0a1549c8ede1395a4c43
MuradAbbaszade <muradabbaszade6@gmail.com>
2023-03-12 21:17:07 +0400

    Variables and mutability

---
+++ basic/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let a = 5; //immutable variable
+    let mut b = 5; //mutable variable
+    const SECONDS :i8 = 60; //immutable variable
+    println!("Variable a={}",a);
+    println!("Variable b={},SECONDS={}",b,SECONDS);
+
+}
---
+++ basic/target/debug/basic.d
@@ -0,0 +1,1 @@
+C:\rust\basic\target\debug\basic.exe: C:\rust\basic\src\main.rs
